4c55c6c17e6a99f90f5fa6495b39a8158a8981ea
ovash <58117780+lolka123123@users.noreply.github.com>
2023-12-03 18:37:36 +0500

    Update loader.py

--- data/loader.py
+++ data/loader.py
@@ -2,8 +2,8 @@ from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
 from database.database import DataBase
 
-# bot = Bot('5226167292:AAFHzB21VU8zGOQ3SKQBke2gyfJNwdtEkIs', parse_mode='HTML')
-bot = Bot('6882651878:AAGArCQjw5J0OU_cRVtBhGg1OzysU_WUfQw', parse_mode='HTML')
+
+bot = Bot('TOKEN', parse_mode='HTML')
 
 db = DataBase()
 
